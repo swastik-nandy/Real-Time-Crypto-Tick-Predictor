e98c3e2604af6e11665eb32ad481f73499d79765
Swastik Nandy <nswastik2003@gmail.com>
2025-08-12 00:42:40 +0530

    New Files

--- data/src/bin/trigger.rs
+++ data/src/bin/trigger.rs
@@ -11,9 +11,9 @@ use data_collection::{cleaner, fetcher};
 
 //------------------------------------CONFIG & CONSTRAINTS--------------------------------------------------------
 
-const MAINT_START: NaiveTime = NaiveTime::from_hms_opt(19, 0, 0).unwrap(); 
-const MAINT_END: NaiveTime = NaiveTime::from_hms_opt(19, 10, 0).unwrap();
-const CLEAN_TIME: NaiveTime = NaiveTime::from_hms_opt(19, 2, 0).unwrap(); 
+const MAINT_START: NaiveTime = NaiveTime::from_hms_opt(5, 0, 0).unwrap(); 
+const MAINT_END: NaiveTime = NaiveTime::from_hms_opt(5, 5, 0).unwrap();
+const CLEAN_TIME: NaiveTime = NaiveTime::from_hms_opt(5, 3, 0).unwrap(); 
 
 const LOOP_TICK: Duration = Duration::from_secs(2);
 const FETCHER_JOIN_TIMEOUT: Duration = Duration::from_secs(10);
